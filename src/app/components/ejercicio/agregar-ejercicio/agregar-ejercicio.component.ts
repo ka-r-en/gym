f45791b4dcc43a5f37b23be2886516a6b15a287a
ka-r-en <krenpaola15@gmail.com>
2021-11-30 02:42:47 -0600

    Update agregar-ejercicio.component.ts

--- src/app/components/ejercicio/agregar-ejercicio/agregar-ejercicio.component.ts
+++ src/app/components/ejercicio/agregar-ejercicio/agregar-ejercicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-ejercicio',
@@ -6,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-ejercicio.component.css']
 })
 export class AgregarEjercicioComponent implements OnInit {
+  formularioDeEjercicios:FormGroup;
 
-  constructor() { }
+  constructor( { 
+    public formulario:FormBuilder,
+    private ruteador:Router
+    ) {
+    
+    this.formularioDeEjercicios=this.formulario.group({
+      IdEjercicio:[''],
+      IdRutina:[''],
+      IdEntrenador:[''],
+      IdCliente:[''],
+      Descripcion:['']
+    })
+   }
 
   ngOnInit(): void {
   }
    enviarDatos():any {
     console.log("Me presionaste");
+     console.log(this.formularioDeEjercicio.value);
+     
+     this.ruteador.navigateByUrl('/ejercicio');
+     
   }
 }
