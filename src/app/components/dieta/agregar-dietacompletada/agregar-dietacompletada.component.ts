09be597c29bd0ffbcf96a4a1675553faefc6eb24
ka-r-en <krenpaola15@gmail.com>
2021-11-30 02:39:52 -0600

    Update agregar-dietacompletada.component.ts

--- src/app/components/dieta/agregar-dietacompletada/agregar-dietacompletada.component.ts
+++ src/app/components/dieta/agregar-dietacompletada/agregar-dietacompletada.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-dietacompletada',
@@ -6,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-dietacompletada.component.css']
 })
 export class AgregarDietacompletadaComponent implements OnInit {
+formularioDeDietasCompletadas:FormGroup;
+  
+  constructor({
+    public formulario:FormBuilder,
+    private ruteador:Router
+  )  }
 
-  constructor() { }
-
+  this.formularioDeAvances=this.formulario.group({
+      IdDietaCompletada:[''],
+      IdDieta:[''],
+      IdCliente:[''],
+      Informe:['']
+    })
+   }
+  
   ngOnInit(): void {
   }
   enviarDatos():any {
     console.log("Me presionaste");
+    console.log(this.formularioDeAvances.value);
+    
+    this.ruteador.navigateByUrl('/DietaCompletada');
   }
 }
