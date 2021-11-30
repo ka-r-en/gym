b2108fb06da140e2fd6308773bc26de9ff855eb9
ka-r-en <krenpaola15@gmail.com>
2021-11-30 02:33:18 -0600

    Update agregar-avance.component.ts

--- src/app/components/avance/agregar-avance/agregar-avance.component.ts
+++ src/app/components/avance/agregar-avance/agregar-avance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-avance',
@@ -6,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-avance.component.css']
 })
 export class AgregarAvanceComponent implements OnInit {
+  formularioDeClientes:FormGroup;
 
-  constructor() { }
+  constructor({
+  public formulario:FormBuilder,
+  private ruteador:Router
+  ) }
+  
+  this.formularioDeAvances=this.formulario.group({
+    IdCliente:[''],
+      Nombre:[''],
+      Edad:[''],
+      Sexo:[''],
+      Peso_inicial:[''],
+      Peso_meta:[''],
+      Talla_inicial:[''],
+      Talla_meta:['']
+    })
+   }
 
   ngOnInit(): void {
   }
   enviarDatos():any {
     console.log("Me presionaste");
+    
+    this.ruteador.navigateByUrl('/avance');
   }
 }
