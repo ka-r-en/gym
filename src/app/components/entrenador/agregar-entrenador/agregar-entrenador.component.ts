38e4c2c917fe85c0f0ec68e42900a8b7e6c9892b
ka-r-en <krenpaola15@gmail.com>
2021-11-30 02:45:58 -0600

    Update agregar-entrenador.component.ts

--- src/app/components/entrenador/agregar-entrenador/agregar-entrenador.component.ts
+++ src/app/components/entrenador/agregar-entrenador/agregar-entrenador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-entrenador',
@@ -6,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-entrenador.component.css']
 })
 export class AgregarEntrenadorComponent implements OnInit {
+  formularioDeEntrenadores:FormGroup;
 
-  constructor() { }
+  constructor(
+    public formulario:FormBuilder,
+    private ruteador:Router
+  )}
+    this.formularioDeEntrenadores=this.formulario.group({
+      IdEntrenador:[''],
+      IdCliente:[''],
+      Nombre:[''],
+      Sexo:[''],
+      Experiencia:[''],
+      Telefono:['']
+    })
+   }
+  
 
   ngOnInit(): void {
   }
   enviarDatos():any {
     console.log("Me presionaste");
+    console.log(this.formularioDeEntrenadores.value);
+    
+    this.ruteador.navigateByUrl('/entrenador');
   }
 }
